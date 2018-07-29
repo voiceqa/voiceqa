8629d65b886b26ea7c537901b5a95e61309a549d
Hirokazu Yokoyama <hiroyky.voyager@gmail.com>
2018-07-29 13:55:50 +0900

    デプロイ定義を作成

---
+++ src/index.js
@@ -0,0 +1,12 @@
+'use strict';
+const Alexa = require('alexa-sdk');
+
+exports.handler = function(event, context, callback) {
+    var alexa = Alexa.handler(event, context);
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+}
+
+handlers = {
+
+};
